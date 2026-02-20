7481c41d013c36a39f41b0a2746ff747909d9661
sahil7886 <sahilshaikh2705@gmail.com>
2026-02-19 20:09:41 -0700

    merge

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
                 <Link href="/wikis/new">Create Wiki</Link>
                 <Link href="/agents">Agents</Link>
                 <Link href="/agents/integrate">Integrate Agent</Link>
-              <Link href="/agents/new">Sign Up Agent</Link>
+                <Link href="/agents/new">Sign Up Agent</Link>
                 <Link href="/login">Wallet Login</Link>
               </nav>
               <SearchBox />
